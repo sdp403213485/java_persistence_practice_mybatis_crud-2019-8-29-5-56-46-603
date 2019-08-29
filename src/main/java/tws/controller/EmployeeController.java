2a885f9407888df1321cbc0240de72ed6f073140
sdp <403213485@qq.com>
2019-08-29 16:19:31 +0800

    1

--- src/main/java/tws/controller/EmployeeController.java
+++ src/main/java/tws/controller/EmployeeController.java
@@ -2,8 +2,11 @@ package tws.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,5 +28,20 @@ public class EmployeeController {
     public ResponseEntity<List<Employee>> getAll() {
         return ResponseEntity.ok(employeeMapper.selectAll());
     }
+    
+	@PostMapping
+	public void addEmployee(@RequestBody Employee employee) {
+		employeeMapper.insertEmployee(employee);
+	}
+	
+	@PutMapping("/{id}")
+	public void updateEmployee(@PathVariable int id,@RequestBody Employee employee){
+		employeeMapper.updateEmployee(id, employee);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deleteEmployee(@PathVariable int id) {
+		employeeMapper.deleteEmployee(id);
+	}
 
 }
--- src/main/java/tws/repository/EmployeeMapper.java
+++ src/main/java/tws/repository/EmployeeMapper.java
@@ -1,6 +1,12 @@
 package tws.repository;
 
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
 import tws.entity.Employee;
 
 import java.util.List;
@@ -8,4 +14,10 @@ import java.util.List;
 @Mapper
 public interface EmployeeMapper {
     List<Employee> selectAll();
+    
+	void insertEmployee(@Param("employee") Employee employee);
+	
+	int updateEmployee(@Param("id") int id, @Param("employee") Employee employee);
+
+	int deleteEmployee(@Param("id") int id);
 }
